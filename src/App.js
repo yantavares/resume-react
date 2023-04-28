60c1ccd606ed8173bb6293185d83c870a0c74339
Yan Tavares <yantdo1@gmail.com>
2023-04-28 11:15:07 -0300

    resume

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <div className="welcome-style-left"></div>
+        <h1 className="welcome-msg">@yantavares</h1>
+        <a className="welcome-link" href="https://github.com/yantavares">
+          (On github)
         </a>
+        <div className="welcome-style-right"></div>
       </header>
     </div>
   );
