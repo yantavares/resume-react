b7fa31f9b5f2fa98a6a6137c710ce50a81b3f76c
yantavares <yantdo1@gmail.com>
2023-05-02 20:14:30 -0300

    added glitched solution for button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./Button.css";
 function Button({ setIsSet, msg, state }) {
+  const [isActive, setIsactive] = useState(false);
   return (
     <>
       <button
         onClick={() => {
+          setIsactive(!isActive);
           setIsSet((prev) => {
             return prev === state ? 0 : state;
           });
         }}
-        className="btn hs"
+        className={(isActive && "btn-active btn") || "btn"}
       >
         {msg}
       </button>
