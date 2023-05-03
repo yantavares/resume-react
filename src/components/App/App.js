160e94e83926ba37efaf58b684ac606f09b69435
yantavares <yantdo1@gmail.com>
2023-05-03 15:42:32 -0300

    removed eye

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,13 +6,11 @@ import { useState } from "react";
 import Projects from "../Projects/Projects";
 import Links from "../Links/Links";
 import waves from "../../images/layered-waves-haikei.svg";
-import Eye from "../Eye/Eye";
 function App() {
   const [isSet, setIsSet] = useState(0);
   return (
     <div className="app">
       <Header />
-      <Eye />
       <section className="buttons">
         <Button isSet={isSet} setIsSet={setIsSet} msg={"ABOUT"} state={1} />
         <Button isSet={isSet} setIsSet={setIsSet} msg={"PROJECTS"} state={2} />
