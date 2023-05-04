ebf6b7d6a89061a716557647bda8df5f32f0243c
yantavares <yantdo1@gmail.com>
2023-05-03 23:10:03 -0300

    added gopher easteer egg

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import unb from "../../images/UnB.png";
 import DevIcon from "devicon-react-svg";
 
 function About() {
-  const [isActive, setIsActive] = useState(0);
+  const [isActive, setIsActive] = useState(1);
   return (
     <div className="about-container">
       <div className="about-2 about">
@@ -38,16 +38,6 @@ function About() {
         <div className="buttons">
           <button
             className={
-              isActive === 0
-                ? "btn btn-active btn-translate"
-                : "btn btn-translate btn-translate-not-active"
-            }
-            onClick={() => setIsActive(0)}
-          >
-            🇧🇷 pt/br
-          </button>
-          <button
-            className={
               isActive === 1
                 ? "btn btn-active btn-translate"
                 : "btn btn-translate btn-translate-not-active"
@@ -56,31 +46,46 @@ function About() {
           >
             en/us 🇺🇸
           </button>
+          <button
+            className={
+              isActive === 0
+                ? "btn btn-active btn-translate"
+                : "btn btn-translate btn-translate-not-active"
+            }
+            onClick={() => setIsActive(0)}
+          >
+            🇧🇷 pt/br
+          </button>
         </div>
         {isActive === 0 && (
-          <p className="about-text">
-            PORTUGUES ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum eros ante, ultricies id mollis vel, imperdiet ut lorem.
-            Ut venenatis vestibulum turpis. Vestibulum congue congue neque,
-            tempor hendrerit ipsum. Suspendisse luctus imperdiet dolor non
-            sodales. Phasellus aliquet, diam eget feugiat congue, neque neque
-            euismod dolor, in sollicitudin diam ante ut eros. Quisque rutrum mi
-            eu rutrum cursus. Mauris eget auctor velit. Nullam massa erat,
-            malesuada ac bibendum sed, consequat vel odio. Mauris finibus dui
-            sit amet luctus auctor.
-          </p>
+          <>
+            <p className="about-text">
+              Sou estudante de Engenharia de Computação da Universidade de
+              Brasília. Sou formado em inglês, morei por um ano na Califórnia e
+              já trabalhei como tradutor de inglês por muitos anos. Minhas
+              habilidades incluem programação, inglês, matemática, física e
+              música.
+            </p>
+            <p className="about-text">
+              O presente site é um currículo feito com as ferramentas React.js,
+              HTML5, CSS3 e JavaScript. Fique à vontade para me mandar e-mails e
+              sugestões!
+            </p>
+          </>
         )}
         {isActive === 1 && (
-          <p className="about-text">
-            EN ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            eros ante, ultricies id mollis vel, imperdiet ut lorem. Ut venenatis
-            vestibulum turpis. Vestibulum congue congue neque, tempor hendrerit
-            ipsum. Suspendisse luctus imperdiet dolor non sodales. Phasellus
-            aliquet, diam eget feugiat congue, neque neque euismod dolor, in
-            sollicitudin diam ante ut eros. Quisque rutrum mi eu rutrum cursus.
-            Mauris eget auctor velit. Nullam massa erat, malesuada ac bibendum
-            sed, consequat vel odio. Mauris finibus dui sit amet luctus auctor.
-          </p>
+          <>
+            <p className="about-text">
+              I'm a Computer Engineering student at the University of Brasilia.
+              I have a degree in English, I lived in California for a year and I
+              have worked as an English translator for many years. My skills
+              include programming, English, math, physics and music.
+            </p>
+            <p>
+              The present site is a curriculum made with React.js, HTML5, CSS3
+              and JavaScript tools. Feel free to send me emails and suggestions!
+            </p>
+          </>
         )}
       </div>
       <div className="about-3 about">
