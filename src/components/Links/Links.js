3fa61ba5ddc6884a7df43b5c6941d0b67e3e4d50
yantavares <yantdo1@gmail.com>
2023-05-04 22:24:07 -0300

    fixed icon size

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -10,27 +10,27 @@ import yan from "../../images/yan.png";
 function Links() {
   return (
     <div className="links">
-      <div class="card-client">
+      <div className="card-client">
         <div className="user-picture">
           <img src={yan} alt="yan (me)" />
         </div>
-        <p class="name-client">
+        <p className="name-client">
           {" "}
           Yan Tavares de Oliveira
           <span>Computer Engineering</span>
         </p>
-        <div class="social-media">
+        <div className="social-media">
           <a href="https://github.com/yantavares">
             <FontAwesomeIcon icon={faGithub} size="2x" />
-            <span class="tooltip-social">Github</span>
+            <span className="tooltip-social">Github</span>
           </a>
           <a href="https://www.instagram.com/yantavares__/">
             <FontAwesomeIcon icon={faInstagram} size="2x" />
-            <span class="tooltip-social">Instagram</span>
+            <span className="tooltip-social">Instagram</span>
           </a>
           <a href="https://www.linkedin.com/in/yantavares01/">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            <span class="tooltip-social">LinkedIn</span>
+            <span className="tooltip-social">LinkedIn</span>
           </a>
         </div>
         <div className="email">
